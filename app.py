9fbcd29891bcdf5d6f35d4827f6a3cb08025b10c
bossware14 <bossware14@gmail.com>
2024-12-18 19:21:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ import gpiod
 import subprocess
 import os
 import signal
+import json
+from datetime import datetime, timezone, timedelta
+
+tz = timezone(timedelta(hours = 7))
+
+
 
 # ติดตั้ง
 # pip3 install flask flask_cors selenium
 # วิธีรัน  python3 main.py
 # เริ่มต้น 
 # http://localhost:3000/start
-# API 1-5 mode
-# 
+# API 1-6 mode
+
 # ตั้งค่า port
 
 API_PORT = 3000 #ห้ามแก้ไข
@@ -28,21 +34,24 @@ def StartServer():
 # ฟังชั้น การทำงาน (API)
 # SET ตั้งค่าสายไฟ
 # https://youtu.be/W_kdEPdpt8Q
+
+#เปิดและปิด 0.1 วิ
 def SETLED(number):
  try:
   LED_PIN = number
   chip = gpiod.Chip('gpiochip4')
   led_line = chip.get_line(LED_PIN)
   led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-  led_line.set_value(1) #เปิด
-  time.sleep(1)
-  led_line.set_value(0) #ปิด
+  led_line.set_value(1)
+  time.sleep(.1)
+  led_line.set_value(0)
   led_line.release()
   chip.close()
   return f"success"
  except:
   return f"error"
 
+#เปิด-ปิด
 def LEDDELAY(number):
  try:
   LED_PIN = number#17 ขาจ่ายไฟ
@@ -60,25 +69,28 @@ def LEDDELAY(number):
   chip.close()
   return f"error"
 
+#ปิด
 def LEDSTOP(number):
  try:
   LED_PIN = number
   chip = gpiod.Chip('gpiochip4')
   led_line = chip.get_line(LED_PIN)
   led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-  led_line.set_value(0) #ปิด
+  led_line.set_value(0)
+  led_line.release()
   chip.close()
   return f"success"
  except:
   return f"error"
 
+#เปิดค้าง
 def LEDSTART(number):
  try:
   LED_PIN = number
   chip = gpiod.Chip('gpiochip4')
   led_line = chip.get_line(LED_PIN)
   led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-  led_line.set_value(1) #ปิด
+  led_line.set_value(1)
   led_line.release()
   chip.close()
   return f"success"
@@ -91,6 +103,15 @@ def LEDSTART(number):
 #อนุญาติ forder = html
 app = Flask(__name__,template_folder="html")
 CORS(app)
+json_data = {}
+
+ID_COMPUTER = subprocess.check_output('hostnamectl').decode().split()[8]
+IP_COMPUTER = requests.get('https://checkip.amazonaws.com').text.strip()
+
+json_data['id'] = ID_COMPUTER
+json_data['ip'] = IP_COMPUTER
+json_data['date'] = datetime.now(tz=tz).strftime('%Y-%m-%d %H:%M:%S')
+json_data['status'] = 'ONLINE'
 
 #ห้ามแก้ไข แจ้งเตือนข้อผิดพลาด
 @app.errorhandler(500)
@@ -111,14 +132,16 @@ def start_template():
 def ExitApp():
     os.system("fuser -k 3000/tcp")
 
+#ปิดแอป
 @app.route('/close',methods=['GET'])
 def KULLSS():
     os.system("pkill chromium")
     msg = {}
     msg['msg'] = 'CLOSE' 
-    #ExitApp()
+    ExitApp()
     return jsonify(msg),200
 
+#เปิดหน้าใหม่
 @app.route('/reload',methods=['GET'])
 def RELOAD():
     os.system("pkill chromium")
@@ -126,6 +149,7 @@ def RELOAD():
     msg = {}
     msg['msg'] = 'RELOAD'
     return jsonify(msg),200
+
 #
 @app.route('/start',methods=['GET'])
 def RUNAPP():
@@ -155,9 +179,7 @@ def start_run():
 # URL 1
 @app.route('/delay',methods=['GET'])
 def start_delay():
-    # รับ on จาก query parameters
     url = request.args.get('id')
-    # ตรวจสอบว่า on ถูกต้อง
     if not url:
         return jsonify({"status": "error"}), 200
     msg = {}
@@ -177,7 +199,7 @@ def stop_run():
     LEDSTOP(18)
     msg = {}
     msg['status'] = "success"
-    msg['msg'] = "ปิกทั้งหมด"
+    msg['msg'] = "ปิดทั้งหมด"
     return jsonify(msg),200
 # URL 1
 @app.route('/on',methods=['GET'])
@@ -198,7 +220,87 @@ def on_run():
 def Ngo():
   os.system("ngrok http http://localhost:3000")
 
-StartServer()
+
+def UPDATE_API(data):
+        if not data:
+            return jsonify({"status": "error"}), 200
+        json_data['data'] = data
+        with open('data.json', 'w') as f:
+          json.dump(json_data, f)
+        return json_data
+
+@app.route('/api',methods=['GET','POST'])
+def WEB_API():
+    if request.method == "POST" :
+        data = json.loads(request.data) 
+        if data['timeout'] is None:
+            return jsonify({"status": "error"}), 200
+
+        mins = int(data['timeout'])
+        update = timezone(timedelta(hours = 7,minutes=int(data['timeout'])))
+        json_data['data'] = data
+        json_data['port'] = API_PORT
+        json_data['data']['update'] = datetime.now(tz=tz).strftime('%Y-%m-%d %H:%M:%S')
+        json_data['data']['time'] = datetime.now(tz=tz).strftime('%H:%M:%S')
+        json_data['data']['timeout'] = datetime.now(tz=update).strftime('%H:%M:%S')
+        json_data['data']['action'] = int(mins)
+        json_data['data']['sec'] = int(mins)*60
+        json_data['data']['runtime'] = '00:00:00'
+        json_data['data']['persen'] = '0' 
+        #json_data['data']['status'] = 'ONLINE'
+        with open('data.json', 'w') as f:
+            json.dump(json_data, f) 
+        return jsonify(json_data),200
+    
+    if request.method == "GET" :
+        if os.path.isfile('data.json'):
+            with open('data.json', 'r') as f:
+                data = json.load(f)
+            data['data']['time'] = datetime.now(tz=tz).strftime('%H:%M:%S')
+            data['status'] = 'ONLINE'
+            t1 = data['data']['timeout'].split(':')
+            t2 = data['data']['time'].split(':')
+            HOUR = int(t1[0]) - int(t2[0])
+            MIN = int(t1[1]) - int(t2[1])
+            SEC = int(t1[2]) - int(t2[2])
+            xper = HOUR-MIN-SEC
+            if xper <= 0 :
+               data['data']['persen'] = 100
+            #else:
+               #data['data']['persen'] = xper*100/100*60#int(data['data']['timeout'].replace(':',''))*100/(int(data['data']['time'].replace(':',''))*60)
+            TOSEC = 0;
+            if HOUR > 0:
+              TOSEC = TOSEC + int(HOUR*60)
+            if MIN > 0:
+              TOSEC = TOSEC + int(MIN*60)
+            if SEC >= 0:
+              TOSEC = TOSEC + int(SEC)
+            else:
+              TOSEC = TOSEC + int(SEC)
+            data['data']['runtime'] = str(HOUR)+':'+str(MIN)+':'+str(SEC)
+            data['data']['runtime'] = datetime.fromtimestamp(TOSEC).strftime('%M:%S')
+            if HOUR <= 0 and MIN <= 0 and SEC <= 0:
+              data['data']['runtime'] = "00:00:00"
+              data['data']['timeout'] = "00:00:00"
+              data['data']['msg'] = "ว่าง"
+            else:
+              data['data']['persen'] = 100-TOSEC*100/int(data['data']['sec'])
+              data['data']['TIMSEC'] = TOSEC
+              data['data']['msg'] = "กำลังทำงาน"
+    
+    return jsonify(data['data']),200
+ 
+
+@app.route('/backend',methods=['GET','POST'])
+def BACKEND_MAIN():
+    if request.method == "GET" :
+        if os.path.isfile('data.json'):
+            with open('data.json', 'r') as f:
+                data = json.load(f)
+            return jsonify(data),200
+
+os.system("pkill chromium")
+StartServer() 
 # ทำงาน
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=API_PORT, debug=DEBUG_MODE)
